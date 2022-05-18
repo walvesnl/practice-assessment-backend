313d1c9a4cf255b2e6bdd53b9c019b2bf7f4edbe
w.alvesnl <w.alvesnl@gmail.com>
2022-05-18 17:27:10 +0200

    feat/models_and_relations_setup

---
+++ models/space.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class space extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      space.belongsTo(models.user, { foreignKey: "userId" });
+      space.hasMany(models.story, { foreignKey: "spaceId" });
+    }
+  }
+  space.init(
+    {
+      title: { type: DataTypes.STRING, allowNull: false },
+      description: DataTypes.TEXT,
+      backgroundColor: DataTypes.STRING,
+      color: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "space",
+    }
+  );
+  return space;
+};
